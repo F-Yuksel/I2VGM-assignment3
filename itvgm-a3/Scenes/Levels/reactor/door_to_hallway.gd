506fe2a2ea95845f88c916ca9202e0a4daf65d7e
Santosh <santoshramlagan@hotmail.com>
2024-12-09 16:56:27 +0100

    reaktor kamir

---
+++ itvgm-a3/Scenes/Levels/reactor/door_to_hallway.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var playerName: String = "Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$".".connect("body_entered", Callable(self, "_on_body_entered"))
+	
+func _on_body_entered(body):
+	if body.name == playerName:
+		Global.doorPosition = Vector2(1700,-45)
+		get_tree().change_scene_to_file("res://Scenes/Levels/reactor/reactor_hallway.tscn")
