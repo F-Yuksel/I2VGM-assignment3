04d273476f63c4b4daec8bfba34a3f4434ed228a
BuildTools <unconfigured@null.spigotmc.org>
2025-01-07 21:34:31 +0100

    Final ButtonMaze Finished

--- itvgm-a3/Scripts/Global.gd
+++ itvgm-a3/Scripts/Global.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var elecwires_solved = false
+var elecbreakers_solved = false
+var electrical_allsolved = false
+
 var doorPosition: Vector2 = Vector2.ZERO
 var hasCodePaper: bool = false
 var hasSuit: bool = false
