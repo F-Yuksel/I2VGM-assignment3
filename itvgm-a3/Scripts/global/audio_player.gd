0c7f65a6fed3c9b17b8a28ecba44ff33eed05629
BuildTools <unconfigured@null.spigotmc.org>
2025-01-09 23:14:21 +0100

    Lol

--- itvgm-a3/Scripts/global/audio_player.gd
+++ itvgm-a3/Scripts/global/audio_player.gd
@@ -2,14 +2,25 @@ extends AudioStreamPlayer
 
 
 const level_music = preload("res://Imports/Music/BackgroundMusic.mp3")
+const intro_music = preload("res://Imports/Music/Lab-Practicum.mp3")
 
-func _play_music(music: AudioStream, volume = -100.0):
+var current_music: AudioStream = null
+
+func _play_music(music: AudioStream, volume: int):
+	# Avoid restarting the same music
 	if stream == music:
 		return
 		
+	# Switch to new music
 	stream = music
-	volume_db = 0.1
+	volume_db = volume
 	play()
 
 func play_music_level():
-	_play_music(level_music)
+	_play_music(level_music, -24)
+	
+func play_intromusic():
+	_play_music(intro_music, -24)
+	
+func play_menumusic():
+	pass
---
+++ itvgm-a3/Scripts/global/test_scene.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Global.elecwires_solved:
+		AudioPlayer.play_music_level()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
