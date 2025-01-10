46cda6a6df05df2bf3069b313591df97a85aa7a2
ValentijnNoelOuwehand <valentijnnoel@hotmail.com>
2025-01-10 13:55:43 +0100

    fixed dev test

--- itvgm-a3/Scripts/global/Global.gd
+++ itvgm-a3/Scripts/global/Global.gd
@@ -3,15 +3,15 @@ extends Node
 var elecwires_solved = false
 var elecbreakers_solved = false
 var elecbuttons_solved = false
-var electrical_allsolved = true
+var electrical_allsolved = false
 var poweron_played = false
 var doorPosition: Vector2 = Vector2.ZERO
 var hasCodePaper: bool = false
 var hasSuit: bool = false
 var generatorOn: bool = false
 var inventoryLock: bool = false
-var reactorOn: bool = true
-var messageSent: bool = true
+var reactorOn: bool = false
+var messageSent: bool = false
 var coordsGet: bool = false
 
 func reset() -> void:
